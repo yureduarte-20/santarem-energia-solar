479a352a8c19e950c77932e4ba35969bcd950d95
Yure Samarone Gomes Duarte <yuresamarone34@gmail.com>
2024-12-08 19:36:19 -0300

    fill pedidos

--- app/Models/Pedido.php
+++ app/Models/Pedido.php
@@ -2,9 +2,44 @@
 
 namespace App\Models;
 
+use App\Enums\TipoRede;
 use Illuminate\Database\Eloquent\Model;
 
 class Pedido extends Model
 {
-    //
+    protected $fillable = [
+        'numero',
+        'data_pedido',
+        'previsao_entrega',
+        'data_entrega',
+        'user_id',
+        'qtde_contratado',
+        'qtde_entregue',
+        'cliente_id', 
+        'valor_contratual',
+        'valor',
+        'descricao',
+        'tipo_rede',
+        'entregue',
+    ];
+    protected $casts = [
+        'tipo_rede' => TipoRede::class,
+        'data_entrega' => 'date:Y-m-d',
+        'previsao_entrega' => 'date:Y-m-d',
+        'data_pedido' => 'date:Y-m-d',
+    ];
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function cliente()
+    {
+        return $this->belongsTo(Cliente::class);
+    }
+    public function pedido_documentos()
+    {
+        return $this->hasMany(PedidoDocumento::class);
+    }
+
 }
